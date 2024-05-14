d2925d0da8392b4c5fb3bac81ea8316f380a0d86
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-05-14 10:58:24 -0300

    Resolução Exercício Soma Simples

---
+++ Nivel1/Soma_Simples/Program.cs
@@ -0,0 +1,8 @@
+﻿int ValorA, ValorB, Soma;
+
+ValorA = int.Parse(Console.ReadLine());
+ValorB = int.Parse(Console.ReadLine());
+Soma = ValorA + ValorB;
+
+Console.WriteLine("SOMA = " + Soma);
+Console.Read();

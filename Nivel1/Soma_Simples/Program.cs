6a59f2fa36614d02baa64835729b124e4b724d9b
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-05-14 11:05:32 -0300

    comentário adicionado

--- Nivel1/Soma_Simples/Program.cs
+++ Nivel1/Soma_Simples/Program.cs
@@ -1,4 +1,7 @@
-﻿int ValorA, ValorB, Soma;
+﻿//Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA.
+//A seguir escrever o valor desta variável.
+
+int ValorA, ValorB, Soma;
 
 ValorA = int.Parse(Console.ReadLine());
 ValorB = int.Parse(Console.ReadLine());

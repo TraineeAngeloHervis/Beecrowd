0eae59028207e61b6066ab8409115a06fc064dc2
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-05-22 15:03:55 -0300

    exercício realizado

---
+++ 1007_Diferenca/CalcularDiferenca.cs
@@ -0,0 +1,7 @@
+public class CalcularDiferenca
+{
+    public int TirarDiferenca(int A, int B, int C, int D)
+    {
+        return (A * B) - (C * D);
+    }
+}
---
+++ 1007_Diferenca/Program.cs
@@ -0,0 +1,16 @@
+﻿//Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA.
+//A seguir escrever o valor desta variável.
+
+Console.WriteLine("Digite o 1° valor: ");
+var A = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o 2° valor: ");
+var B = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o 3° valor: ");
+var C = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o 4° valor: ");
+var D = int.Parse(Console.ReadLine());
+
+CalcularDiferenca calculo = new();
+int resultado = calculo.TirarDiferenca(A, B, C, D);
+
+Console.WriteLine("DIFERENCA = " + resultado);
